59e01dfd8258517470796858b1b25a9d39ffe2b0
Rangga Darmajati <rangga.android69@gmail.com>
2021-10-01 23:35:15 +0700

    create replace phone number function

--- app/Helper/Utils.js
+++ app/Helper/Utils.js
@@ -2,7 +2,12 @@
 'use strict'
 const Database = use('Database')
 class Utils {
-    
+    replacePhoneNumber(value) {
+        String.prototype.replaceAt = function (index, replacement) {
+            return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+        }
+        return '6' + value.replaceAt(0, "2");
+    }
 }
 
 module.exports = new Utils
