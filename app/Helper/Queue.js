a51800ce5a2a3ec52b75a6a1f7e3b7d223f9a376
Rangga Darmajati <rangga.android69@gmail.com>
2021-10-13 11:55:51 +0700

    add bee-queue pakage & create queue for email delivery

---
+++ app/Helper/Queue.js
@@ -0,0 +1,45 @@
+
+'use strict'
+const Env = use('Env')
+const Event = use('Event')
+const Queue = require('bee-queue');
+const options = {
+    getEvents:true,
+    removeOnSuccess: true,
+    redis: {
+        host: Env.get('REDIS_HOST', 'localhost'),
+        port: Env.get('REDIS_PORT', '6379')
+    },
+}
+const queue = new Queue('EMAIL_DELIVERY', options);
+
+class BeQueue {
+    emailQueue(obj) {
+        const job = queue.createJob(obj);
+        job.retries(3);
+        job.save();
+        job.on('progress', (progress) => {
+            console.log(`Job ${job.id} reported progress: ${progress}%`);
+        });
+        job.on('succeeded', (result) => {
+            console.log(`Received result for job ${job.id}: Successfully!`);
+        });
+        job.on('retrying', (err) => {
+            console.log(
+                `Job ${job.id} failed with error ${err.message} but is being retried!`
+            );
+        });
+        job.on('failed', (err) => {
+            console.log(`Job ${job.id} failed with error ${err.message}`);
+        });
+    }
+}
+
+// Process jobs from as many servers or processes as you like
+queue.process((job, done) => {
+    console.log(`Processing job ${job.id} | job send email to: ${job.data.email}`);
+    Event.fire('new::user', job.data);
+    done();
+});
+
+module.exports = new BeQueue
